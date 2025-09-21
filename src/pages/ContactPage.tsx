e4ea107100a5ca3a8cf9237bffdff08e01cae731
Shaurya Tyagi <shauryaiiitd@gmail.com>
2025-09-21 16:29:17 +0530

    Update ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -450,7 +450,7 @@ const ContactPage: React.FC = () => {
             <h2 className="text-2xl font-bold text-gray-800 mb-6 font-serif">Visit Our Gallery</h2>
             <div className="aspect-video bg-gray-200 rounded-xl overflow-hidden shadow-lg">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13972.72219811516!2d78.1642496!3d29.9574825!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d3f1c5f5f5b5b%3A0x5a5a5a5a5a5a5a5a!2sKankhal%2C+Haridwar%2C+Uttarakhand%2C+India!5e0!3m2!1sen!2sin!4v1632748285000!5m2!1sen!2sin"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3495.123456789012!2d78.167890123!3d29.953456789!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d3f1c6e7f8a5b%3A0x123456789abcdef!2sPurushottam%20Vihar%2C%20Haridwar%2C%20Uttarakhand%2C%20India!5e0!3m2!1sen!2sin!4v1699999999999!5m2!1sen!2sin"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
@@ -494,5 +494,6 @@ const ContactPage: React.FC = () => {
 };
 
 export default ContactPage;
+
 
 
